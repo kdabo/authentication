9de60935faa3b0697fcea2e79576b60bd223de23
katarina <katarina.dabo@gmail.com>
2018-12-03 16:48:39 +0100

    action refactor

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,9 +2,9 @@ import { AUTH_USER } from "./types";
 
 // redux-thunk allows me to take control over dispatch process
 // by using redux-thunk we can return either object or function from action creators
-// whereas redux-promise
-export const signup = ({ email, password }) => {
-  return function (dispatch) {
-    dispatch({ type: AUTH_USER })
-  }
+// it is possible to make as many requests as needed and only when the data is returned,
+// when there is validaiton completed, there is an option to dispatch action which will rerun reducers and update the state of application
+// whereas redux-promise returns one action which has to have a promise on payload property
+export const signup = ({ email, password }) => dispatch => {
+
 };
